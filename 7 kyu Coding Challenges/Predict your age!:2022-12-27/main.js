a38a3922deadf98e81ae6e1aeee74ac492c77bb7
wendy barrios <wbarrios45@yahoo.com>
2022-01-05 11:12:44 -0800

    Solution for 1-05-22

---
+++ 7 kyu Coding Challenges/Predict your age!:2022-12-27/main.js
@@ -0,0 +1,27 @@
+// My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+
+// In honor of my grandfather's memory we will write a function using his formula!
+
+// Take a list of ages when each of your great-grandparent died.
+// Multiply each number by itself.
+// Add them all together.
+// Take the square root of the result.
+// Divide by two.
+// Example
+
+// predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
+
+
+
+function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
+  //   map -> el * el
+  //   reduce -> sum
+  //   square root
+  //   divide
+    
+    
+    let newArr = [age1,age2,age3,age4,age5,age6,age7,age8]
+    let newNums = newArr.map(el => el * el).reduce((a,b) => a + b,0)
+    
+    return Math.floor(Math.sqrt(newNums) / 2)
+  }
