6a14a990e83350402c26ecbed529248954a8dfe3
wendy barrios <wbarrios45@yahoo.com>
2022-02-14 18:57:31 -0800

    Coding Challenge and solution for 2-14-22 coding challenge

---
+++ 8 kyu Coding Challenges/Basic Data Types: 2022-02-14/app.js
@@ -0,0 +1,23 @@
+// Give you a function animal, accept 1 parameter:obj like this:
+// {name:"dog",legs:4,color:"white"}
+// and return a string like this:
+// "This white dog has 4 legs."
+
+// Return: 
+// "This black cat has 4 legs"
+// "This gray rabbit has 4 legs"
+// "This brown kangaroo has 2 legs"
+
+function animal(obj){
+   
+    return `This ${obj.color} ${obj.name} has ${obj.legs} legs.`
+    }
+
+
+
+
+
+
+
+
+
