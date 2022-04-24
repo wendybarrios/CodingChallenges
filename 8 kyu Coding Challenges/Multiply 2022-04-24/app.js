2a3b5449db1bfa5401e3fcf83baa786a51aaec34
wendy barrios <wbarrios45@yahoo.com>
2022-04-24 13:47:40 -0700

    Coding Challenge and solution for 4-24-22 coding challenge

---
+++ 8 kyu Coding Challenges/Multiply 2022-04-24/app.js
@@ -0,0 +1,16 @@
+// This code does not execute properly. Try to figure out why.
+// function multiply(a, b){
+//     a * b
+//   }
+
+// R: return the product of a and b
+
+function multiply(a,b){ 
+    return a * b
+}
+
+// Test cases
+
+// console.log(multiply (3,6)) should return 18
+// console.log(multiply (2,10)) should return 20
+// console.log(multiply (7,11)) should return 77
