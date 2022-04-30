76080d70825e779a9fbcd15d806319d18a06c69e
wendy barrios <wbarrios45@yahoo.com>
2022-04-29 19:04:57 -0700

    Coding Challenge and Solution 4-29-22

---
+++ 8 kyu Coding Challenges/Reversed String: 2022-04-29/main.js
@@ -0,0 +1,21 @@
+// Complete the solution so that it reverses the string passed into it.
+/*
+function solution(str){
+}
+
+*/
+
+/* Tests
+'hello' => 'olleh'
+'world'  =>  'dlrow'
+'word'   =>  'drow'
+*/
+
+// Return given string reversed
+
+/* Solution
+function solution(str){
+  return str.split('').reverse().join ('')
+}
+
+*/
