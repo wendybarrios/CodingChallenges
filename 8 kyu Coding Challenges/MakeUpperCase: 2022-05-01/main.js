47f05911f72d02e1f5f15f0674a7ce1cacbdde66
wendy barrios <wbarrios45@yahoo.com>
2022-05-01 20:21:06 -0700

    Coding Challenge and Solution for 5-01-22

---
+++ 8 kyu Coding Challenges/MakeUpperCase: 2022-05-01/main.js
@@ -0,0 +1,18 @@
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+    // Code here
+  }
+
+// Tests
+/*
+console.log(makeUpperCase('hello')) --> "HELLO"
+console.log(makeUpperCase('blahblah')) --> "BLAHBLAH"
+console.log(makeUpperCase('trex ')) --> "trex"
+*/
+
+// Solution
+function makeUpperCase(str) {
+    // return str to UpperCase 
+    return str.toUpperCase()
+  }
