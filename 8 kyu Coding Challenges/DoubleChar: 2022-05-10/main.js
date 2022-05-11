b69780704053bc2493f97d3ebc673c0f168bc469
wendy barrios <wbarrios45@yahoo.com>
2022-05-10 21:28:53 -0700

    Coding Challenge and Solution for 5-10-22

--- 8 kyu Coding Challenges/DoubleChar: 2022-05-10/main.js
+++ 8 kyu Coding Challenges/DoubleChar: 2022-05-10/main.js
@@ -1,11 +1,19 @@
 // Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
 
-
+function doubleChar(str) {
+    // Your code here
+  }
 
 // Tests
 /*
-doubleChar("abcd")   --> "aabbccdd"
-doubleChar("Adidas") -->  "AAddiiddaass"
-doubleChar("1337")   -->  "11333377"
+console.log(doubleChar("abcd"))   --> "aabbccdd"
+console.log(doubleChar("Adidas")) -->  "AAddiiddaass"
+console.log(doubleChar("1337"))   -->  "11333377"
 
 */
+
+
+// Solutions
+function doubleChar(str) {
+    return str.split('').map(el => el + el).join('')
+  }
