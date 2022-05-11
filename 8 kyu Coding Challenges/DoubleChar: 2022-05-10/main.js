5ea712f4fcc0d7ea987dfeb7a62314567e912bec
wendy barrios <wbarrios45@yahoo.com>
2022-05-10 21:23:12 -0700

    Tests Updated for Coding Challenge 5-10-22

---
+++ 8 kyu Coding Challenges/DoubleChar: 2022-05-10/main.js
@@ -0,0 +1,11 @@
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+
+
+// Tests
+/*
+doubleChar("abcd")   --> "aabbccdd"
+doubleChar("Adidas") -->  "AAddiiddaass"
+doubleChar("1337")   -->  "11333377"
+
+*/
