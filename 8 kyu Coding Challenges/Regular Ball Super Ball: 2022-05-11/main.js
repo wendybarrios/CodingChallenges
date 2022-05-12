bf268bab85e6e8171b40fbb765b54be35742e68c
wendy barrios <wbarrios45@yahoo.com>
2022-05-11 18:49:05 -0700

    Coding Challenge and Solution for 5-11-22

---
+++ 8 kyu Coding Challenges/Regular Ball Super Ball: 2022-05-11/main.js
@@ -0,0 +1,19 @@
+/*
+Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+*/
+
+var Ball = function(ballType) {
+    // your code goes here
+  };
+
+// Tests
+/*
+new Ball().ballType--> "regular"
+new Ball("super").ballType --> "super"
+*/
+
+
+var Ball = function(ballType) {
+    this.ballType = ballType || "regular"
+  };
