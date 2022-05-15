b767ab0a8dd0ec560426ad40311dcf8aa4247562
wendy barrios <wbarrios45@yahoo.com>
2022-05-14 20:15:37 -0700

    Coding Challenge and Solution for 5-14-22

--- 8 kyu Coding Challenges/Power of 2: 2022-05-14/main.js
+++ 8 kyu Coding Challenges/Power of 2: 2022-05-14/main.js
@@ -6,10 +6,19 @@ function powersOfTwo(n){
 // Tests 
 
 /*
-
 console.log(powersOfTwo(0)) --> [1]
 console.log(powersOfTwo(1)) --> [1, 2]
 console.log(powersOfTwo(4)) --> [1, 2, 4, 8, 16]
-
-
 */
+
+// Solution
+
+function powersOfTwo(n){
+  
+    let result=[]
+    // create empty array
+    for(let i=0; i<=n;i++){
+      result.push(Math.pow(2,i))
+    }
+    return result
+  }
