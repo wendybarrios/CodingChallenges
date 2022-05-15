2a408dc81f49205d75563e862dd703a05435cb1e
wendy barrios <wbarrios45@yahoo.com>
2022-05-14 20:12:17 -0700

    Coding Challenge and Tests added

---
+++ 8 kyu Coding Challenges/Power of 2: 2022-05-14/main.js
@@ -0,0 +1,15 @@
+// Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+function powersOfTwo(n){
+  
+}
+
+// Tests 
+
+/*
+
+console.log(powersOfTwo(0)) --> [1]
+console.log(powersOfTwo(1)) --> [1, 2]
+console.log(powersOfTwo(4)) --> [1, 2, 4, 8, 16]
+
+
+*/
