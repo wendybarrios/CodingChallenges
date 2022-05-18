f3c0719c4d92b81db28759e5b27a11cb5deea82c
wendy barrios <wbarrios45@yahoo.com>
2022-05-17 20:35:05 -0700

    Coding Challenge and Solution for 5-17-22

---
+++ 8 kyu Coding Challenges/Volume of a Cuboid: 2022-05-17/main.js
@@ -0,0 +1,18 @@
+// Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+
+// P: numbers, 
+// R: return volume
+
+// Tests
+/*
+getVolumeOfCuboid(1,2,2) --> 4
+getVolumeOfCuboid(6.3,2,5) --> 63
+*/
+
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+        // multiply all numbers together
+      return length * width * height
+    
+    }
+  }
