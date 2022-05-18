490497bfcf77760aee2a096be9fa73f9ab5e6df0
wendy barrios <wbarrios45@yahoo.com>
2022-05-18 12:56:51 -0700

    Coding Challenge and solution for 5-18-22

---
+++ 8 kyu Coding Challenges/Get Planet Name By ID: 2022-05-18/main.js
@@ -0,0 +1,66 @@
+// The function is not returning the correct values. Can you figure out why?
+
+function getPlanetName(id){
+    var name;
+    switch(id){
+      case 1:
+        name = 'Mercury'
+      case 2:
+        name = 'Venus'
+      case 3:
+        name = 'Earth'
+      case 4:
+        name = 'Mars'
+      case 5:
+        name = 'Jupiter'
+      case 6:
+        name = 'Saturn'
+      case 7:
+        name = 'Uranus'
+      case 8:
+        name = 'Neptune'
+    }
+    
+    return name;
+  }
+
+// Tests
+/*
+getPlanetName(2) --> 'Venus'
+getPlanetName(5) --> 'Jupiter'
+getPlanetName(3) -->  'Earth'
+*/
+
+// Solution
+// missing break statements
+function getPlanetName(id){
+    var name;
+    switch(id){
+      case 1:
+        name = 'Mercury'
+        break;
+      case 2:
+        name = 'Venus'
+        break;
+      case 3:
+        name = 'Earth'
+        break; 
+      case 4:
+        name = 'Mars'
+        break;
+      case 5:
+        name = 'Jupiter'
+        break;
+      case 6:
+        name = 'Saturn'
+        break;
+      case 7:
+        name = 'Uranus'
+        break;
+      case 8:
+        name = 'Neptune'
+        break;
+    }
+    
+    return name;
+  }
