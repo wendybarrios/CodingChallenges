b3d4d391000b21cf8e59aa708cc2aebc6ca4aa53
wendy barrios <wbarrios45@yahoo.com>
2022-05-21 19:42:43 -0700

    Coding Challenge and Solution for 5-21-22

--- 8 kyu Coding Challenges/Count Odd Numbers below n: 2022-05-21/main.js
+++ 8 kyu Coding Challenges/Count Odd Numbers below n: 2022-05-21/main.js
@@ -1,8 +1,5 @@
 // Given a number n, return the number of positive odd numbers below n, EASY!
 
-// Prep
-// p: number
-// return number
 
 // Tests
 /*
@@ -10,6 +7,7 @@ oddCount(15)  -->   7
 oddCount(15023) --> 7511
 */
 
+// Solution
 function oddCount (n){
     // Divide by 2 and round down
     return Math.floor(n/2)
---
+++ 8 kyu Coding Challenges/Grasshopper- if/else syntax debug: 2022-05-21/main.js
@@ -0,0 +1,36 @@
+// While making a game, your partner, Greg, decided to create a function to check if the user is still alive called checkAlive/CheckAlive/check_alive. Unfortunately, Greg made some errors while creating the function.
+
+// checkAlive/CheckAlive/check_alive should return true if the player's health is greater than 0 or false if it is 0 or below.
+
+// The function receives one parameter health which will always be a whole number between -10 and 10.
+
+// prep
+// p: whole number
+// return true or false
+
+function checkAlive (health) {
+    if () {
+      health < 0
+      return false
+    } else () {
+      return true
+    }
+  }
+
+// Tests 
+
+/*
+checkAlive(5) -->  true
+checkAlive(0) -->  false
+*/
+
+function checkAlive (health) {
+    // if else statement
+    if (health < 0){
+      return false
+    } else if (health > 0){
+      return true
+    } else if (health == false){
+        return false
+    }
+  }
