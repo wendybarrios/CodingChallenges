4aea7cbc53fcc4cc5919d114347560ba5e6aa893
wendy barrios <wbarrios45@yahoo.com>
2022-05-24 13:00:41 -0700

    Coding Challenge and Solution for 5-24-22

--- 8 kyu Coding Challenges/Dollars and Cents: 2022-05-23/main.js
+++ 8 kyu Coding Challenges/Dollars and Cents: 2022-05-23/main.js
@@ -4,7 +4,6 @@
 
 // The rest of your team will make sure that the argument is sanitized before being passed to your function although you will need to account for adding trailing zeros if they are missing (though you won't have to worry about a dangling period).
 
-
 // Tests
 // formatMoney(39.99) --> '$39.99'
 // formatMoney(3.1)   --> $3.00
