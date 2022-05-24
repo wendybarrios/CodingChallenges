9445f0d55ccee59513ec0eae0cc2ab7302f9e9cd
wendy barrios <wbarrios45@yahoo.com>
2022-05-23 19:17:04 -0700

    Challenge corrected.

--- 8 kyu Coding Challenges/Dollars and Cents: 2022-05-23/main.js
+++ 8 kyu Coding Challenges/Dollars and Cents: 2022-05-23/main.js
@@ -4,8 +4,6 @@
 
 // The rest of your team will make sure that the argument is sanitized before being passed to your function although you will need to account for adding trailing zeros if they are missing (though you won't have to worry about a dangling period).
 
-// p: number, may be decimal point
-// r: reuturn number in dollar sign
 
 // Tests
 // formatMoney(39.99) --> '$39.99'
