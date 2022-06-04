f566af4b7bc1d796a1f5bbee6a339565752f2a0e
wendy barrios <wbarrios45@yahoo.com>
2022-06-03 20:28:27 -0700

    Coding Challenge and Solution for 6-03-22

---
+++ 8 kyu Coding Challenges/Incorrect division method: 2022-06-03/main.js
@@ -0,0 +1,12 @@
+// This method, which is supposed to return the result of dividing its first argument by its second, isn't always returning correct values. Fix it.
+// const solve = (x, y) => x // y
+
+
+// Tests
+// solve(2, 1) --> 2
+// solve(2, 4) --> 0.5
+// solve(0, 8) --> 0
+
+function solve(x, y){
+    return x / y
+   }
