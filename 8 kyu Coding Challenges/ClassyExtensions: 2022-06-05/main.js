3acfa6f8d0fd01a1031fc6458f76dba979c7325d
wendy barrios <wbarrios45@yahoo.com>
2022-06-05 14:31:25 -0700

    Test Cases edited.

--- 8 kyu Coding Challenges/ClassyExtensions: 2022-06-05/main.js
+++ 8 kyu Coding Challenges/ClassyExtensions: 2022-06-05/main.js
@@ -4,8 +4,6 @@
 // Your task is to complete the Cat class which Extends Animal and replace the speak method to return the cats name + meows. e.g. 'Mr Whiskers meows.'
 // The name attribute is passed with this.name (JS), @name (Ruby) or self.name (Python).
 
-// p: strings
-// return string
 
 // Tests
 // cat = new Cat('Lamp') --> cat.speak(),'Lamp meows.'
