2759c4c4d42f33620f4eeb055742acdff188f699
wendy barrios <wbarrios45@yahoo.com>
2022-06-08 09:21:04 -0700

    Coding Challenge and Solution for 6-08-22

---
+++ 8 kyu Coding Challenges/Simple Comparison?: 2022-06-08/main.js
@@ -0,0 +1,16 @@
+// Write a function that will compare two values, one will be a number and one will be a string. Return true if they are the same character (regardless of their different data types) and return false if they are not.
+
+// Tests
+// add('1', 1) --> true
+// add(1, '1') --> true
+// add(1, '0') -->  false
+
+function add (a, b){
+    // if else
+    if(a == b){
+        return true
+    } else {
+        return false
+    }
+    
+}
