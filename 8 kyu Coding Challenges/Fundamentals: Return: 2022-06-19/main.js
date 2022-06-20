614580830a9bc5bc5efaaa94bc9a12b3251928ba
wendy barrios <wbarrios45@yahoo.com>
2022-06-19 21:28:20 -0700

    Coding Challenge and Solution for 6-19-22

---
+++ 8 kyu Coding Challenges/Fundamentals: Return: 2022-06-19/main.js
@@ -0,0 +1,50 @@
+// Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+// Please use the following function names:
+
+// addition = add
+
+// multiply = multiply
+
+// division = divide (both integer and float divisions are accepted)
+
+// modulus = mod
+
+// exponential = exponent
+
+// subtraction = subt
+
+
+
+// Tests
+// add(1,2) --> 3
+// multiply(1,2) --> 2
+// divide(2,1) --> 2
+
+function add(a,b){
+    return a + b
+}
+
+function divide(a,b){
+    return a / b
+}
+
+function multiply(a,b){
+    return a * b
+}
+
+function mod(a,b){
+    return a % b
+}
+   
+function exponent(a,b){
+    return (Math.pow(a, b))
+}
+    
+function subt(a,b){
+    return a - b
+}
+
+
+
+
