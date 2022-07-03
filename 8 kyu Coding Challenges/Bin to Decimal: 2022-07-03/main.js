3776b6f9ee32c993d769624b68d2f16e2229c1e0
wendy barrios <wbarrios45@yahoo.com>
2022-07-03 09:30:29 -0700

    Coding Challenge and Solution for 7-03-22

---
+++ 8 kyu Coding Challenges/Bin to Decimal: 2022-07-03/main.js
@@ -0,0 +1,8 @@
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+// Tests
+// "1001001" --> 73
+
+function binToDec (bin){
+return parseInt (bin, 2)
+}
