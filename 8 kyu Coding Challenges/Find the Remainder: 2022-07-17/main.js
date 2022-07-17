86eaad12da28a9017dade62e5757f2839d62609e
wendy barrios <wbarrios45@yahoo.com>
2022-07-17 12:47:02 -0700

    Coding Challenge and Solution for 7-17-22

---
+++ 8 kyu Coding Challenges/Find the Remainder: 2022-07-17/main.js
@@ -0,0 +1,18 @@
+// Write a function that accepts two integers and returns the remainder of dividing the larger value by the smaller value.
+
+// Division by zero should return NaN.
+
+
+// Tests
+remainder(17,5) --> 2
+remainder(13, 72) -->  7 
+remainder(0, 0) --> NaN
+
+function remainder(n, m){
+    // Divide the larger argument by the smaller argument and return the remainder 
+   if (n > m){
+       return n % m
+   } else {
+       return m % n
+   }
+  }
