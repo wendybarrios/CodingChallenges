fbb80fb53d65641ca5f4ebc70744f4a9a2928302
wendy barrios <wbarrios45@yahoo.com>
2022-07-19 10:11:12 -0700

    Coding Challenge and Solution for 7-19-22

---
+++ 8 kyu Coding Challenges/Counting Sheep: 2022-07-19/main.js
@@ -0,0 +1,18 @@
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+
+// Tests
+[true,  true,  true,  false,
+    true,  true,  true,  true ,
+    true,  false, true,  false,
+    true,  false, false, true ,
+    true,  true,  true,  true ,
+    false, false, true,  true] --> 17
+
+
+
+
+function countSheeps (arrayOfSheep){
+    let total = arrayOfSheep.filter((el => el === true))
+    return total.length
+}
