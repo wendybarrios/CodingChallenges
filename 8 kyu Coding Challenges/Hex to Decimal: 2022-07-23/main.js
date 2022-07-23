adfdb6f581024f35183f19f27cd88bd90a0ff170
wendy barrios <wbarrios45@yahoo.com>
2022-07-23 09:20:18 -0700

    Coding Challenge and Solution for 7-23-22

---
+++ 8 kyu Coding Challenges/Hex to Decimal: 2022-07-23/main.js
@@ -0,0 +1,17 @@
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+
+// Tests
+hexToDec("1") --> 1
+hexToDec("a") -->10
+hexToDec("10") --> 16
+
+
+
+
+function hexToDec(hexString){
+    //your code here
+    return parseInt(hexString, 16 )
+  }
+
+
