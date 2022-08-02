b4ca346e781fd929e62ea9e0bb7df1de78ec191e
wendy barrios <wbarrios45@yahoo.com>
2022-08-02 10:24:13 -0700

    Coding Challenge and Solution for 8-02-11

---
+++ 8 kyu Coding Challenges/Get the Nth Even Number: 2022-08-01/main.js
@@ -0,0 +1,15 @@
+// Return the Nth Even Number
+
+// Example(Input --> Output)
+
+
+
+// Tests
+ nthEven(1) --> 0
+ nthEven(2) --> 2
+ nthEven(3) --> 4
+
+
+function nthEven (n){
+return n * 2 - 2
+}
