62a8157e53a5e8c497b43bbcafe5bce8ccf5ed66
wendy barrios <wbarrios45@yahoo.com>
2022-08-14 11:21:41 -0700

    Coding Challenge and Solution for 8-14-22

---
+++ 8 kyu Coding Challenges/Generate range of integers: 2022-08-14/main.js
@@ -0,0 +1,15 @@
+// Implement a function named generateRange(min, max, step), which takes three arguments and generates a range of integers from min to max, with the step. The first integer is the minimum value, the second is the maximum of the range and the third is the step. 
+
+
+// p: numbers, positive, 
+// r: array, postive, whole
+
+// Tests
+generateRange(2, 10, 2) --> [2,4,6,8,10]
+generateRange(1, 10, 3) // should return array of [1,4,7,10]
+
+
+function generateRange(min, max, step){
+
+
+}
