6122c9f1678dcf2b66292008d4bad815094744a2
wendy barrios <wbarrios45@yahoo.com>
2022-08-23 11:24:04 -0700

    Coding Challenge added on 8-23-22

---
+++ 8 kyu Coding Challenges/No zeroes for heros: 2022-08-23/main.js
@@ -0,0 +1,24 @@
+// Numbers ending with zeros are boring.
+
+// They might be fun in your world, but not here.
+
+// Get rid of them. Only the ending ones.
+
+// p: number, whole
+// r: number
+
+
+// Tests
+noBoringZeros(1450) --> 145
+noBoringZeros(960000) --> 96
+noBoringZeros(1050) --> 105
+
+
+function noBoringZeros(n){
+    // num to string
+    let num = n.toString()
+    // remove
+    let newNum = num.replace(/\0+$/,'')
+    // string to num
+    return 
+  }
