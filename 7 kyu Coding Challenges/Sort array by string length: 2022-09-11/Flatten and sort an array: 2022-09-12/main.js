ee930640fc52c0654695724c1920fe2a28fddb8d
wendy barrios <wbarrios45@yahoo.com>
2022-09-12 13:09:53 -0700

    Solution edited.

--- 7 kyu Coding Challenges/Sort array by string length: 2022-09-11/Flatten and sort an array: 2022-09-12/main.js
+++ 7 kyu Coding Challenges/Sort array by string length: 2022-09-11/Flatten and sort an array: 2022-09-12/main.js
@@ -4,8 +4,6 @@
 
 // Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9].
 
-// p: array
-// r: array
 
 // Tests
 flattenAndSort([]) --> []
@@ -13,11 +11,9 @@ flattenAndSort([[3, 2, 1], [7, 9, 8], [6, 4, 5]]) --> [1, 2, 3, 4, 5, 6, 7, 8, 9
 flattenAndSort([[1, 3, 5], [100], [2, 4, 6]]) --> [1, 2, 3, 4, 5, 6, 100]
 
 function flattenAndSort(array){
-   
         // concat
         let newArr = array.flat()
         // sort
         return newArr.sort((a,b) => a - b, 0)
-
         
 }
