3e509d26a5ee56a637147fa77bdb059a290f9987
wendy barrios <wbarrios45@yahoo.com>
2022-09-22 13:55:59 -0700

    Coding Challenge and Solution for 9-22-22

---
+++ 7 kyu Coding Challenges/Name Array Capping: 2022-09-22/main.js
@@ -0,0 +1,18 @@
+// Create a method that accepts an array of names, and returns an array of each name with its first letter capitalized.
+
+
+// p: array of strings
+// r: array of strings
+
+// Tests
+ capMe(['jo', 'nelson', 'jurie']) -->  ['Jo', 'Nelson', 'Jurie']
+ capMe(['KARLY', 'DANIEL', 'KELSEY']) --> ['Karly', 'Daniel', 'Kelsey']
+
+
+ function capMe(names){
+    //  convert to all letters
+let lowerCaseArr = names.map(el => el.toLowerCase())
+    // uppercase first letter
+let finalArr = lowerCaseArr.map(el => el[0].toUpperCase() + el.slice(1))
+return finalArr
+}
