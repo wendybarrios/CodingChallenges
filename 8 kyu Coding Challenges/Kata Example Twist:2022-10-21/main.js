b62c584707db55b1550d7417f4aae6176caff2af
wendy barrios <wbarrios45@yahoo.com>
2022-10-21 11:48:54 -0700

    Coding Challenge and Solution for 10-21-22

---
+++ 8 kyu Coding Challenges/Kata Example Twist:2022-10-21/main.js
@@ -0,0 +1,9 @@
+// Add the value "codewars" to the array websites/Websites 1,000 times.
+
+// ["codewars, codewars, codewars,..."]
+
+// add the value "codewars" to the websites array 1,000 times
+
+var websites = Array(1000).fill('codewars')
+
+console.log(websites) // ["codewars", codewars, codewars,..."]
