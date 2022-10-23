c0b621986114c27f5a98e773d9f98cb480e64f4d
wendy barrios <wbarrios45@yahoo.com>
2022-10-23 10:03:08 -0700

    Coding Challenge and Solution for 10-23-22

---
+++ 8 kyu Coding Challenges/Get character from ASCII Value:2022-10-23/main.js/main.js
@@ -0,0 +1,10 @@
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+function getChar(c){
+    // charCode
+      return String.fromCharCode(c)
+    }
+    
+    console.log(getChar(55), '7')
+    console.log(getChar(58),':')
+    console.log(getChar(60),'<')
