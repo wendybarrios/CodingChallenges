59df651015c10fc96514a164756e4e2bc3cbf162
wendy barrios <wbarrios45@yahoo.com>
2022-10-25 10:58:18 -0700

    Coding Challenge and Solution for 10-25-22

---
+++ 8 kyu Coding Challenges/Type of sum:2022-10-25/main.js
@@ -0,0 +1,12 @@
+// Return the type of the sum of the two arguments
+
+// typeOfSum(12, 1), 'number';
+// typeOfSum('d', 1), 'string';
+// typeOfSum(1, 'a'), 'string';
+
+function typeOfSum(a, b) {
+    // add together
+    let sumOfTwo= a + b
+    // find typeoOf sum
+    return (typeof sumOfTwo)
+    }
