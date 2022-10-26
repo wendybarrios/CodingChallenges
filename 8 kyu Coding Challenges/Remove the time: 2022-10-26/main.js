f9b8458c3c2527fa1e02229864e032bdaef7e1be
wendy barrios <wbarrios45@yahoo.com>
2022-10-26 12:50:07 -0700

    Coding Challenge and Solution for 10-26-22

---
+++ 8 kyu Coding Challenges/Remove the time: 2022-10-26/main.js
@@ -0,0 +1,20 @@
+// You're re-designing a blog and the blog's posts have the following format for showing the date and time a post was made:
+
+// Weekday Month Day, time e.g., Friday May 2, 7pm
+
+// You're running out of screen real estate, and on some pages you want to display a shorter format, Weekday Month Day that omits the time.
+
+// Write a function, shortenToDate, that takes the Website date/time in its original string format, and returns the shortened format.
+
+// Assume shortenToDate's input will always be a string, e.g. "Friday May 2, 7pm". Assume shortenToDate's output will be the shortened string, e.g., "Friday May 2".
+
+function shortenToDate(longDate) {
+
+  //   split
+    let newDate = longDate.split(" ").slice(0,-1).join(" ").replace(/[,]/g,"")
+    return newDate
+  }
+
+shortenToDate("Friday May 2, 9am"), "Friday May 2";
+shortenToDate("Tuesday January 29, 10pm"), "Tuesday January 29";
+shortenToDate("Monday December 25, 10pm"), "Monday December 25";
