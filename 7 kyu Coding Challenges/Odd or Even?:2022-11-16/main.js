cbe3ca6ffb2e6686de63daa7eb5ce5ae2ac3e936
wendy barrios <wbarrios45@yahoo.com>
2022-11-16 12:24:56 -0800

    Test cases edited.

--- 7 kyu Coding Challenges/Odd or Even?:2022-11-16/main.js
+++ 7 kyu Coding Challenges/Odd or Even?:2022-11-16/main.js
@@ -18,9 +18,9 @@ function oddOrEven (array){
       } else if(totalSum % 2 !== 1){
         return "odd"
       }
-      
+
     }
     
-    // console.log(oddOrEven([0] ), "even")
-    // console.log(oddOrEven([0,1,4]) ,"odd")
-    // console.log( oddOrEven([0,-1,-5]) , "even")
+    console.log(oddOrEven([0] ), "even")
+    console.log(oddOrEven([0,1,4]) ,"odd")
+    console.log( oddOrEven([0,-1,-5]) , "even")
