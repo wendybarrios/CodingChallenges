5066e2168d564495ff72ef7a31be1e813f2268f3
wendy barrios <wbarrios45@yahoo.com>
2022-11-25 18:56:39 -0800

    Coding Challenges and Solution for 11-25-22

---
+++ 8 kyu Coding Challenges/Ensure Question: 2022-11-25/main.js
@@ -0,0 +1,16 @@
+// Given a string, write a function that returns the string with a question mark ("?") appends to the end, unless the original string ends with a question mark, in which case, returns the original string.
+
+
+function ensureQuestion(s) {
+    // Code here\
+    
+  //   split, check if last element == ?, if not push ? to array
+       let newWord = s.split('')
+       
+       if(newWord[newWord.length-1] == "?"){
+         return newWord.join('')
+       } else if (newWord[newWord.length-1] !== "?"){
+         newWord.push("?")
+         return newWord.join('')
+       }
+    }
