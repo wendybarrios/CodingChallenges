16a7f5088a677079c63eed0a7a49803480a435b2
wendy barrios <wbarrios45@yahoo.com>
2023-02-13 10:04:12 -0800

    Coding Challenge and Solution for 2-13-23

---
+++ 7 kyu Coding Challenges/Functional Addition:2023-02-13/main.js
@@ -0,0 +1,19 @@
+// Create a function add(n)/Add(n) which returns a function that always adds n to any number
+
+// Note for Java: the return type and methods have not been provided to make it a bit more challenging.
+
+// create funct -> return n + any num
+// n -> num, no specials chars, not empty
+// num,
+
+
+// add(1)(3) , 4
+
+function add(n){
+
+    return function (m){
+     return  n+m
+    }
+    
+  }
+  
