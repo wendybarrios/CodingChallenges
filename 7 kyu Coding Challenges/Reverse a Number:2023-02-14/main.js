499663b92bf065e0a8ad37f0d015a99e356b6cf6
wendy barrios <wbarrios45@yahoo.com>
2023-02-14 09:39:15 -0800

    Coding Challenge for 2-14-23

---
+++ 7 kyu Coding Challenges/Reverse a Number:2023-02-14/main.js
@@ -0,0 +1,20 @@
+// Given a number, write a function to output its reverse digits. (e.g. given 123 the answer is 321)
+
+// Numbers should preserve their sign; i.e. a negative number should still be negative when reversed.
+
+function reverseNumber(n){
+
+    let checkZero = n.toString().split('')
+    if(n===0){
+      return 0
+    } else if (n < 0){
+      let newNum = Math.abs(n).toString().split('').reverse().join('')
+      return Number(-newNum)
+    } else if (checkZero[checkZero.length-1] === 0){
+      let newString = n.toString().replace(/'0'/g, "")
+      return newString
+    } else {
+      return Number(n.toString().split('').reverse().join(''))
+    }
+    
+  }
