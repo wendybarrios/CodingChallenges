28484339e56a447b1c30fe1600d61adb4f53062a
wendy barrios <wbarrios45@yahoo.com>
2023-02-15 12:26:11 -0800

    Coding Challenge and Solution for 2-15-23

---
+++ 7 kyu Coding Challenges/Sum of odd numbers:2023-02-15/main.js
@@ -0,0 +1,17 @@
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// function: sum of numbers in the nth row
+// nums, odd, no strings, no special chars, not empty
+// number
+
+// 1 , 1
+// 42, 74088
+
+function rowSumOddNumbers(n) {
+	return n*n*n
+}
+
+
+
+console.log(rowSumOddNumbers(1), 1)
+console.log(rowSumOddNumbers(42), 74088)
