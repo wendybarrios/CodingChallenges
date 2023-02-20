32156522e7defca1500b8f1cd47d0b94be2b3049
wendy barrios <wbarrios45@yahoo.com>
2023-02-20 09:27:04 -0800

    Coding Challenge and Solution for 2-20-23

---
+++ 7 kyu Coding Challenges/Odd-Even String Sort:2023-02-20/main.js
@@ -0,0 +1,25 @@
+// Given a string s. You have to return another string such that even-indexed and odd-indexed characters of s are grouped and groups are space-separated (see sample below)
+
+// Note: 
+// 0 is considered to be an even index. 
+// All input strings are valid with no spaces
+
+
+function sortMyString(S){
+    // split -> loop thru the arr and push to evenArr or oddArr
+    // return string
+    let evenArr = []
+    let oddArr = []
+    let newArr = S.split('')
+    for(let i=0; i < newArr.length;i++){
+      if(i%2===0){
+        evenArr.push(newArr[i])
+      } else {
+        oddArr.push(newArr[i])
+      }
+    }
+  
+    return evenArr.join('') + " " + oddArr.join('')
+   
+  
+  }
