074dfe37671063c754f753b95c113a71a01cf157
wendy barrios <wbarrios45@yahoo.com>
2023-02-21 18:00:58 -0800

    Coding Challenge and Solution for 2-21-23

---
+++ 7 kyu Coding Challenges/Coding Meetup #2 - Higher-Order Functions Series - Greet developers:2-23-02-21/main.js
@@ -0,0 +1,45 @@
+// You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+// Your task is to return an array where each object will have a new property 'greeting' with the following string value:
+
+// Hi < firstName here >, what do you like the most about < language here >?
+
+// For example, given the following input array:
+
+// var list1 = [
+//   { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+//   { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+// ];
+// your function should return the following array:
+
+// [
+//   { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java',
+//     greeting: 'Hi Sofia, what do you like the most about Java?'
+//   },
+//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python',
+//     greeting: 'Hi Lukas, what do you like the most about Python?'
+//   },
+//   { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby',
+//     greeting: 'Hi Madison, what do you like the most about Ruby?'
+//   } 
+// ];
+// Notes:
+
+// The order of the properties in the objects does not matter (except in COBOL).
+// The input array will always be valid and formatted as in the example above.
+
+
+// function: return an array of objects with a new property greeting
+// array of objects: each object will have properties, not empty, always an array
+// array of objects -> w new property
+
+function greetDevelopers(list){
+    // loop thru list -> add greeting property (name + language)
+      for(let obj of list){
+        obj["greeting"] = `Hi ${obj.firstName}, what do you like the most about ${obj.language}?`
+      }
+     console.log(list)
+      return list
+     
+    }
