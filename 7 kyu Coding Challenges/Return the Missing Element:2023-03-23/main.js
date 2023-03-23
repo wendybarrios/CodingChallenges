e94bd91c741444d8283cd6f8767a545f09e2d032
wendy barrios <wbarrios45@yahoo.com>
2023-03-23 10:41:34 -0700

    Test Cases added.

--- 7 kyu Coding Challenges/Return the Missing Element:2023-03-23/main.js
+++ 7 kyu Coding Challenges/Return the Missing Element:2023-03-23/main.js
@@ -21,3 +21,7 @@ function getMissingElement(superImportantArray){
        }
      }
   }
+
+
+  console.log(getMissingElement( [0,5,1,3,2,9,7,6,4]), 8);
+console.log(getMissingElement( [9,2,4,5,7,0,8,6,1]), 3)
