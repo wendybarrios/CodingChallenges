208b9084492dfa9a1bcec8ecb6024b53a184675a
wendy barrios <wbarrios45@yahoo.com>
2023-04-10 13:34:27 -0700

    Solution added

--- 6 kyu Coding Challenges/Persistent Bugger: 2023-04-10/main.js
+++ 6 kyu Coding Challenges/Persistent Bugger: 2023-04-10/main.js
@@ -5,3 +5,16 @@
 // 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
 // 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
 // 4 --> 0 (because 4 is already a one-digit number)
+
+
+
+function persistence(num) {
+   let digits = num.toString()
+   let counter = 0
+
+   while(digits.length > 1){
+    digits = digits.split('').map(el => +el).reduce((a,c) => a*c).toString()
+    counter++
+   }
+   return counter
+ }
