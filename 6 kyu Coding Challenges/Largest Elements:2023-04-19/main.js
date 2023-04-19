1a97ce2e0a8eb1b7fe6821727d5b6e394d075c50
wendy barrios <wbarrios45@yahoo.com>
2023-04-19 08:46:08 -0700

    Solution added

--- 6 kyu Coding Challenges/Largest Elements:2023-04-19/main.js
+++ 6 kyu Coding Challenges/Largest Elements:2023-04-19/main.js
@@ -4,3 +4,18 @@
 
 // largest(2, [7,6,5,4,3,2,1])
 // // => [6,7]
+
+
+function largest(n, array) {
+    // Find the n highest elements in a list
+  // sort in ascending
+  //  slice(-n) 
+    
+    let newArr = array.sort((a,b) => a - b)
+    let final = newArr.slice(-n)
+    if(n===0){
+      return []
+    } 
+    return final
+  
+  }
