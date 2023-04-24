6faad8148651df5ae763dfb3144124ed999df15e
wendy barrios <wbarrios45@yahoo.com>
2023-04-24 16:30:11 -0700

    Solution added.

--- 7 kyu Coding Challenges/Sum of Odd Cubed Numbers:2023-04-24/main.js
+++ 7 kyu Coding Challenges/Sum of Odd Cubed Numbers:2023-04-24/main.js
@@ -1,1 +1,20 @@
 // Find the sum of the odd numbers within an array, after cubing the initial integers. The function should return undefined if any of the values aren't numbers.
+
+
+
+function cubeOdd(arr) {
+    let emptyArr = []
+    
+  for(let i=0; i < arr.length;i++){
+    if(typeof arr[i] !== "number" ){
+      return undefined
+    }
+  }
+    
+  for(let i=0; i < arr.length;i++){
+    if(arr[i] % 2 !== 0){
+      emptyArr.push(Math.pow(arr[i],3))
+    }
+  }
+    return emptyArr.reduce((a,b) => a + b)
+  }
