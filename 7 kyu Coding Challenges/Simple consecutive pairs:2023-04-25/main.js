764f2c3239514714d9da4c00658a1369cfcc2c0b
wendy barrios <wbarrios45@yahoo.com>
2023-04-25 18:28:45 -0700

    Solution #2 added.

--- 7 kyu Coding Challenges/Simple consecutive pairs:2023-04-25/main.js
+++ 7 kyu Coding Challenges/Simple consecutive pairs:2023-04-25/main.js
@@ -38,3 +38,17 @@ function pairs(ar){
      return count
      
    };
+
+// Solution #2
+function pairs(ar){
+
+    let otherArr = ar.map(el => Math.abs(el))
+    let count = 0
+    for(let i=0; i < otherArr.length;i+=2){
+       if(Math.abs(arr[i] - arr[i+1]) === 1){
+        count++
+       }
+    }
+
+    return count
+   };
