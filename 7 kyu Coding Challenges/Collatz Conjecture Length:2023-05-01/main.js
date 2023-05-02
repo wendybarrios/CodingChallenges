78540c230c94456ac830e2cba52c4710d60fffbc
wendy barrios <wbarrios45@yahoo.com>
2023-05-01 17:10:56 -0700

    Solution added.

--- 7 kyu Coding Challenges/Collatz Conjecture Length:2023-05-01/main.js
+++ 7 kyu Coding Challenges/Collatz Conjecture Length:2023-05-01/main.js
@@ -10,3 +10,16 @@
 // [ 20, 10, 5, 16, 8, 4, 2, 1 ]
 // Write a program that will output the length of the Collatz Conjecture for any given n.
 // In the example above, the output would be 8.
+
+function collatz(n) {
+    let count = 1
+    while (n>1){
+      count++
+      if(n % 2 === 0){
+        n = n/2
+      } else {
+        n = (n*3)+1
+      }
+    }
+    return count
+  }
