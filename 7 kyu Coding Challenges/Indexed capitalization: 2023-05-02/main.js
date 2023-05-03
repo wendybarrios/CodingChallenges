62df942efba204d1886ed5d739ce611b258c70ef
wendy barrios <wbarrios45@yahoo.com>
2023-05-02 18:29:54 -0700

    Solution added.

--- 7 kyu Coding Challenges/Indexed capitalization: 2023-05-02/main.js
+++ 7 kyu Coding Challenges/Indexed capitalization: 2023-05-02/main.js
@@ -5,3 +5,18 @@
 // capitalize("abcdef",[1,2,5]) = "aBCdeF"
 // capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
 // The input will be a lowercase string with no spaces and an array of digits.
+
+
+function capitalize(s,arr){
+    let newString = s.split('')
+  //   loop thru arr -> reassign indexOf in newString
+    for(let i=0; i < arr.length;i++){
+       if(arr[i] > s.length){
+        newString[arr[i]] = newString[arr[i]]
+       } else {
+          newString[arr[i]] = newString[arr[i]].toUpperCase()
+       } 
+    }
+  
+  return newString.join('')
+  };
