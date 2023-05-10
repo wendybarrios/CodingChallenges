233e371eb986f21029fb3272502aa5828c0e88de
wendy barrios <wbarrios45@yahoo.com>
2023-05-09 17:56:08 -0700

    Solution added.

--- 7 kyu Coding Challenges/Regexp Basics - is it a vowel?:2023-05-09/main.js
+++ 7 kyu Coding Challenges/Regexp Basics - is it a vowel?:2023-05-09/main.js
@@ -1,1 +1,13 @@
 // Implement the function which should return true if given object is a vowel (meaning a, e, i, o, u, uppercase or lowercase), and false otherwise.
+
+
+
+String.prototype.vowel = function() {
+    let final = this
+    if(final.toLowerCase() === "a" || final.toLowerCase() === "e" ||final.toLowerCase() === "i"||final.toLowerCase() === "o" || final.toLowerCase() === "u"){   
+    return true
+    } else {
+      return false
+    }
+    
+   };
