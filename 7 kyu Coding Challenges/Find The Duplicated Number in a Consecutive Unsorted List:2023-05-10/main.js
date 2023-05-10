d985cd7106db8adfd6b700113d3d1013fbf66d74
wendy barrios <wbarrios45@yahoo.com>
2023-05-10 12:56:37 -0700

    Test Cases Added

--- 7 kyu Coding Challenges/Find The Duplicated Number in a Consecutive Unsorted List:2023-05-10/main.js
+++ 7 kyu Coding Challenges/Find The Duplicated Number in a Consecutive Unsorted List:2023-05-10/main.js
@@ -9,3 +9,7 @@
 function findDup( arr ){
     return arr.find(el => arr.indexOf(el) !== arr.lastIndexOf(el));
   }
+
+
+  console.log(findDup([1,2,2,3]), 2);
+  console.log(findDup([1,3,2,5,4,5,7,6]), 5)
