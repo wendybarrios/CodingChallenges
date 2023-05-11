6024d6b94f1e5f4addf60da096e183110e2e39c2
wendy barrios <wbarrios45@yahoo.com>
2023-05-11 10:55:46 -0700

    Coding Challenge and Solution for 5-11-23

---
+++ 7 kyu Coding Challenges/Product Of Maximums Of Array (Array Series #2):2023-05-11/main.js
@@ -0,0 +1,14 @@
+// Task
+
+// Given an array/list [] of integers , Find the product of the k maximal numbers.
+
+// Notes
+
+// Array/list size is at least 3 .
+
+// Array/list's numbers Will be mixture of positives , negatives and zeros
+
+// Repetition of numbers in the array/list could occur.
+
+
+
