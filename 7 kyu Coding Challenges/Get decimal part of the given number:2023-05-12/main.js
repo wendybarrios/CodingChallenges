ff0047f319b52797eda4627cc68c2a7816f728a9
wendy barrios <wbarrios45@yahoo.com>
2023-05-14 13:26:38 -0700

    Solution added.

--- 7 kyu Coding Challenges/Get decimal part of the given number:2023-05-12/main.js
+++ 7 kyu Coding Challenges/Get decimal part of the given number:2023-05-12/main.js
@@ -6,3 +6,16 @@
 
 // getDecimal(2.4)  === 0.4
 // getDecimal(-0.2) === 0.2
+
+
+function getDecimal(n){
+    //convert toString -> split by . -> get arr[1] -> return that num
+    
+    if(Number.isInteger(n)){
+      return 0
+    }
+    
+  let newArr = n.toString().split('.')
+  let num = newArr[1]
+  return Number(`0.${num}`)
+  }
