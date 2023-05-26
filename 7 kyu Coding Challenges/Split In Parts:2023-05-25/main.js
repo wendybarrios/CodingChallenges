2c855be1e0b9e40150a18c5928d089abbb33aeab
wendy barrios <wbarrios45@yahoo.com>
2023-05-26 15:08:14 -0700

    Coding Challenge for 5-26-23

--- 7 kyu Coding Challenges/Split In Parts:2023-05-25/main.js
+++ 7 kyu Coding Challenges/Split In Parts:2023-05-25/main.js
@@ -25,3 +25,6 @@ var splitInParts = function(s, partLength){
   }
 
 //   Test Cases
+console.log(splitInParts("supercalifragilisticexpialidocious", 3), "sup erc ali fra gil ist ice xpi ali doc iou s")
+console.log(splitInParts("HelloKata", 1), "H e l l o K a t a")
+console.log(splitInParts("HelloKata", 9), "HelloKata")
