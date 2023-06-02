4f101e6067ee65c4dd584671aa3e22cf7ef9d4b9
wendy barrios <wbarrios45@yahoo.com>
2023-06-02 14:41:48 -0700

    Coding Challenge for 6-02-23

--- 7 kyu Coding Challenges/Find min and max:2023-05-31/main.js
+++ 7 kyu Coding Challenges/Find min and max:2023-05-31/main.js
@@ -1,3 +1,13 @@
 // Implement a function that returns the minimal and the maximal value of a list (in this order).
 
 
+function getMinMax(arr){
+    //Math.min in var
+  //   Math.max in var
+  //   create Arr
+    
+  let max = Math.max(...arr)
+  let min = Math.min(...arr)
+  
+  return [min,max]
+  };
