70a7e69ac2b218d9e21c63c803fc4b08368a8262
wendy barrios <wbarrios45@yahoo.com>
2023-06-04 09:58:48 -0700

    Coding Challenge for 6-04-23

---
+++ 7 kyu Coding Challenges/See You Next Happy Year:2023-06-04/main.js
@@ -0,0 +1,14 @@
+// You're saying good-bye your best friend , See you next happy year .
+
+// Happy Year is the year with only distinct digits , (e.g) 2018
+
+// Task
+
+// Given a year, Find The next happy year or The closest year You'll see your best friend !alt!alt
+
+// Notes
+
+// Year Of Course always Positive .
+// Have no fear , It is guaranteed that the answer exists .
+// It's not necessary that the year passed to the function is Happy one .
+// Input Year with in range (1000  ≤  y  ≤  9000)
