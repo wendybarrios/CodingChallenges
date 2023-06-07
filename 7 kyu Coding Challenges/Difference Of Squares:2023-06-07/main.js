5d3e8c79f534bdb945300dc46f57b39649c1fd28
wendy barrios <wbarrios45@yahoo.com>
2023-06-07 13:42:36 -0700

    Solution added.

--- 7 kyu Coding Challenges/Difference Of Squares:2023-06-07/main.js
+++ 7 kyu Coding Challenges/Difference Of Squares:2023-06-07/main.js
@@ -15,3 +15,21 @@
 // Hence the difference between square of the sum of the first ten natural numbers and the sum of the squares of those numbes is: 3025 - 385 = 2640
 
 
+function differenceOfSquares(n){
+    // ...
+  //   square of sum of nums
+  //   loop from 1 to num
+  //   get sum (Reduce) -> square that num
+  //   sum of square of nums
+  //   map -> el * el  -> find sum
+    
+    
+    let newArr = []
+    for(let i=1; i <= n; i++){
+      newArr.push(i)
+    }
+    
+    let first = Math.pow(newArr.reduce((a,b) => a+b,0), 2)
+    let second = newArr.map(el => el*el).reduce((a,b)=>a+b,0)
+    return first - second
+  }
