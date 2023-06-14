681a46afd071b31e8a62c91b91027b6935b33e25
wendy barrios <wbarrios45@yahoo.com>
2023-06-13 19:21:11 -0700

    Coding Challenge for 6-13-23

---
+++ 7 kyu Coding Challenges/Squares sequence:2023-06-13/main.js
@@ -0,0 +1,4 @@
+// Complete the function that returns an array of length n, starting with the given number x and the squares of the previous number. If n is negative or zero, return an empty array/list.
+
+
+
