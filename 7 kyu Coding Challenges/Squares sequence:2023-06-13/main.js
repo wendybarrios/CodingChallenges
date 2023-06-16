2b60a2595861ce6add502e3c8c31f6bbebe4bda4
wendy barrios <wbarrios45@yahoo.com>
2023-06-16 10:02:14 -0700

    Coding Challenge added.

--- 7 kyu Coding Challenges/Squares sequence:2023-06-13/main.js
+++ 7 kyu Coding Challenges/Squares sequence:2023-06-13/main.js
@@ -2,3 +2,13 @@
 
 
 
+function squares(x, n) {
+    if( n<=0){
+      return []
+    }
+   let newArr = [x]
+   for(let i=1; i < n;i++){
+     newArr.push(newArr[i-1]*newArr[i-1])
+   }
+   return newArr
+ }
